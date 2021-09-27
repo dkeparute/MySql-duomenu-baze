d8a0106c0c218f8fd14a7921ff670919c2d283d8
Dovilė <dovile.keparute@gmail.com>
2021-09-27 11:59:42 +0300

    VEIKIA!

--- src/run.js
+++ src/run.js
@@ -84,66 +84,69 @@ function printResults({ results, fields }) {
 
 // PROGRAMA:
 
-// let choise;
-// while (choise !== 0) {
-//     console.log("1. Visi zmones");
-//     console.log("2. Naujas zmogus");
-//     console.log("3. Istrinti zmogu");
-//     // console.log("4. Visi zmogaus adresai");
-//     // console.log("5. Naujas zmogaus adresas");
-//     // console.log("6. Istrinti zmogaus adresa");
-//     // console.log("7. Visi zmogaus kontaktai");
-//     // console.log("8. Naujas zmogaus kontaktas");
-//     // console.log("9. Istrinti zmogaus kontakta");
-//     console.log("0. Baigti");
-//     choise = parseInt(await input("Pasirink: "));
-//     let conn;
-//     let r;
-//     try {
-//         conn = await connect();
-//         switch (choise) {
-//             case 1:
-//                 r = await query(conn, "select * from zmones",);
-//                 printResults(r);
-//                 break;
-//             case 2:
-//                 let vardas = await input("Ivesk varda: ");
-//                 let pavarde = await input("Ivesk pavarde: ");
-//                 let gimimo_data = await input("Ivesk gimimo data ('YYYY-MM-DD'): ");
-//                 let alga = await input("Iveska alga: ");
-//                 r = await query(
-//                     conn, "insert into zmones (vardas,pavarde, gimimo_data, alga) values (?,?,?,?);", [vardas, pavarde, gimimo_data, alga]);
-//                 break;
-//             case 3:
-//                 let id = await input("Ivesk id: ");
-//                 id = parseInt(id);
-//                 r = await query(conn, "update zmones set gimimo_data=?, vardas=? where id=?;",
-//                     [gimimo_data, pav, id]);
-//                 break;
-//             // case 4:
-//             // case 5:
-//             // case 6:
-//             // case 7:
-//             // case 8:
-//             // case 9:
-//             // case 0:
-//         }
-//     } catch (err) {
-//         console.log("Klaida", err);
-//     } finally {
-//         if (conn) {
-//             try {
-//                 await end(conn);
-//             } catch (e) {
-//                 // ignored
-//             }
-//         }
-//     }
-// }
+let choise;
+while (choise !== 0) {
+    console.log("1. Visi zmones");
+    console.log("2. Naujas zmogus");
+    console.log("3. Istrinti zmogu");
+    // console.log("4. Visi zmogaus adresai");
+    // console.log("5. Naujas zmogaus adresas");
+    // console.log("6. Istrinti zmogaus adresa");
+    // console.log("7. Visi zmogaus kontaktai");
+    // console.log("8. Naujas zmogaus kontaktas");
+    // console.log("9. Istrinti zmogaus kontakta");
+    console.log("0. Baigti");
+    choise = parseInt(await input("Pasirink: "));
+    let conn;
+    let r;
+    try {
+        conn = await connect();
+        switch (choise) {
+            case 1:
+                r = await query(conn, "select * from zmones",);
+                printResults(r);
+                break;
+            case 2:
+                let vardas = await input("Ivesk varda: ");
+                let pavarde = await input("Ivesk pavarde: ");
+                let gimimo_data = await input("Ivesk gimimo data ('YYYY-MM-DD'): ");
+                let alga = await input("Iveska alga: ");
+                r = await query(
+                    conn, "insert into zmones (vardas,pavarde, gimimo_data, alga) values (?,?,?,?);", [vardas, pavarde, gimimo_data, alga]);
+                break;
+            case 3:
+             
+                break;
+            // case 4:
+            // case 5:
+            // let id = await input("Ivesk id: ");
+            // id = parseInt(id);
+            // r = await query(conn, "update zmones set gimimo_data=?, vardas=? where id=?;",
+            //     [gimimo_data, pav, id]);
+            //     break;
+            // case 6:
+            // case 7:
+            // case 8:
+            // case 9:
+            // case 0:
+        }
+    } catch (err) {
+        console.log("Klaida", err);
+    } finally {
+        if (conn) {
+            try {
+                await end(conn);
+            } catch (e) {
+                // ignored
+            }
+        }
+    }
+}
 
 
 
-// VEIKIA
+// ATNAUJINAME INFORMACIJA - VEIKIA
+
 // let id = await input("Ivesk id: ");
 // id = parseInt(id);
 // let pav = await input("Ivesk kita pavadinima: ");
@@ -171,7 +174,8 @@ function printResults({ results, fields }) {
 //   }
 // }
 
-// VEIKIA
+// ISTRINAME INFORMACIJA - VEIKIA
+
 // let id = await input("Ivesk id: ");
 // id = parseInt(id);
 // let conn;
@@ -195,21 +199,19 @@ function printResults({ results, fields }) {
 //   }
 // }
 
-// IVEDAME NAUJA INFORMACIJA - NESIGAUNA!!!!!!!!!!!!!!!!!!!!!!!
-// ________________________________________________________________________________
+// IVEDAME NAUJA INFORMACIJA - VEIKIA
 
-// DESTYTOJO PVZ:
 // let pav = await input("Ivesk pavadinima: ");
-// let data = await input("Ivesk data: ");
-// data = new Date(data);
+// let pavarde = await input("Ivesk pavarde: ");
+// let alga = await input("Ivesk alga: ");
 
 // let conn;
 // try {
 //   conn = await connect();
 //   const r = await query(
 //     conn,
-//     "insert into cekiai (data, parduotuve) values (?, ?);",
-//     [data, pav],
+//     "insert into zmones (pavarde, vardas, alga) values (?, ?,?);",
+//     [pavarde, pav, alga],
 //   );
 //   console.log(r);
 // } catch (err) {
@@ -224,37 +226,8 @@ function printResults({ results, fields }) {
 //   }
 // }
 
-// MANO:
+// ATSPAUSDINAME INFO IS SQL - VEIKIA
 
-let pav = await input("Ivesk pavadinima: ");
-let gimimo_data = await input("Ivesk gimimo data ('YYYY-MM_DD'): ");
-
-gimimo_data = new Date(gimimo_data)
-
-let conn;
-try {
-  conn = await connect();
-  const r = await query(
-    conn,
-    "insert into zmones (gimimo_data, vardas) values (?, ?);",
-    [gimimo_data, pav],
-  );
-  console.log(r);
-} catch (err) {
-  console.log("Klaida", err);
-} finally {
-  if (conn) {
-    try {
-      await end(conn);
-    } catch (e) {
-      // ignored
-    }
-  }
-}
-
-
-// ATSPAUSDINAME INFO IS SQL
-// VEIKIA
 // let pav = await input("Ivesk ID: ");
 // pav = "%" + pav + "%";
 // // NIEKADA SITAIP NEREIKTU RASYTI UZKLAUSOS, NES GALES ATAKUOTI HAKERIAI
@@ -277,5 +250,5 @@ try {
 //         }
 //     }
 // }
-console.log("pabaiga");
+console.log("Pabaiga");
 rl.close();
